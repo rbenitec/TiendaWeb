c83156bf7e1ae6d01459af2bd91799f04334ec17
rbenitec <105337433+rbenitec@users.noreply.github.com>
2022-07-13 08:46:40 -0500

    Update

---
+++ src/main/java/com/example/rbenitec/springbootapp/TiendaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbenitec.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaWebApplication.class, args);
+	}
+
+}
